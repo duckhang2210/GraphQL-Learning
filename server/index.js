ccb96c3fa8bd0773cba8f8b4b37c95c24ca38ac2
unknown <duckhang2210@gmail.com>
2021-09-12 23:37:25 -0500

    Creared Resolvers and Schema

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require('apollo-server');
+const { mainCards, animals, categories } = require('./db');
+const typeDefs = require('./schema');
+const Query = require('./resolvers/Query');
+const Animal = require('./resolvers/Animal');
+const Category = require('./resolvers/Category');
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers: {
+		Query,
+		Animal,
+		Category
+	}
+});
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+	console.log(`🚀  Server ready at ${url}`);
+});
